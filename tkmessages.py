76c4dda5bbb490c79f2b87e504552be58a849644
WildCoder <WildCoder+GitHub@GMail.com>
2025-05-22 10:03:30 -0500

    Added joystick message types and replaced auto with auto() so they are assigned distinct values.

--- tkmessages.py
+++ tkmessages.py
@@ -2,15 +2,17 @@ from enum import Enum, auto
 from typing import Callable
 
 class TKMsgType(Enum):
-    ENABLE_STATUS_MSG  = auto
-    DISABLE_STATUS_MSG = auto
-    DISABLE_THREE_DOT  = auto
-    CACHE_BUILD_COMPLETED = auto
-    SHUTDOWN = auto
+    ENABLE_STATUS_MSG  = auto()
+    DISABLE_STATUS_MSG = auto()
+    DISABLE_THREE_DOT  = auto()
+    CACHE_BUILD_COMPLETED = auto()
+    SHUTDOWN = auto()
+    JOY_BUTTON_DOWN = auto()
+    JOY_BUTTON_UP = auto()
+    JOY_AXIS_MOTION = auto()
     
 class TkMsg():
-    def __init__(self, MsgType: TKMsgType, func: Callable, msg: str):
+    def __init__(self, MsgType: TKMsgType, func: Callable = None, payload: dict = None):
         self.msgType = MsgType
         self.call = func
-        self.msg = msg
-
+        self.payload = payload if payload is not None else {}
