3a46e55a908b21e55050f421de68a07ef0aef4af
Superhac <superhac007@gmail.com>
2025-07-31 08:26:07 -0500

    privatize functions and cleam up vpinfe-core.js

--- web/common/vpinfe-core.js
+++ web/common/vpinfe-core.js
@@ -12,51 +12,22 @@ class VPinFECore {
     this.gamepadEnabled = true;
   }
 
+  // ***********************************
+  // Public api
+  // ***********************************
+
   init() {
-    // Set up other event listeners
+    // Set up other event listeners (TODO) - NOT USED
     window.addEventListener('keydown', (e) => this.onKeyDown(e));
 
     // Wait for pywebview then run all async init
     window.addEventListener('pywebviewready', async () => {
-      await this.onPyWebviewReady();  // Wait until everything is done
+      await this.#onPyWebviewReady();  // Wait until everything is done
       this._resolveReady();           // Now we're truly ready
     });
-
-    
   }
 
-  // Init functions
-
-  async onPyWebviewReady() {
-    console.log("pywebview is ready!");
-    await this.loadMonitors();
-    await this.getTableData();
-    
-    // only run on the table window.. Its the master controller for all screens/windows
-    if(await vpin.call("get_my_window_name") == "table") {
-      await this.initGamepadMapping();
-      this.updateGamepads();           // No await needed here — runs loop
-    }
-  }
-
-  async getTableData() {
-    this.tableData = JSON.parse(await window.pywebview.api.get_tables());
-  }
-
-  async loadMonitors() {
-    this.monitors = await window.pywebview.api.get_monitors();
-  }
-
-  async initGamepadMapping () {
-    const joymap = await this.call("get_joymaping");
-    this.joyButtonMap = Object.fromEntries(
-      Object.entries(joymap).map(([key, val]) => [val, key])
-    );
-  }
-
-  // *******************************************
   // register for events
-
   async registerInputHandler(handler) {
     windowName = await this.call("get_my_window_name");
     if (typeof handler === 'function' && windowName == "table" ) {
@@ -65,14 +36,105 @@ class VPinFECore {
     }
   }
 
-  // *******************************************
-  // Gamepad and Keyboard Processing to handlers
-
-  handleJoystickInput(event) {
-    this.inputHandlers.forEach(handler => handler(event));
+  // Keybaord input processing to handlers (TODO)
+  onKeyDown(e) {
+    if (e.key === 'Escape' && window.pywebview) {
+      window.pywebview.api.close_app();
+    }
   }
 
-  updateGamepads() {
+  async call(method, ...args) {
+    if (window.pywebview && window.pywebview.api && typeof window.pywebview.api[method] === 'function') {
+      return await window.pywebview.api[method](...args);
+    } else {
+      const errMsg = `Method ${method} does not exist on window.pywebview.api`;
+      console.error(errMsg);
+      throw new Error(errMsg);
+    }
+  }
+
+  // get table image url paths
+  getImageURL(index, type) {
+    const table = this.tableData[index];
+    if (type == "table") {
+      this.call("console_out", this.#convertImagePathToURL(table.TableImagePath) )
+      return this.#convertImagePathToURL(table.TableImagePath);
+    }
+    else if (type == "bg") {
+      this.call("console_out", this.#convertImagePathToURL(table.BGImagePath) )
+      return this.#convertImagePathToURL(table.BGImagePath);
+
+    } 
+    else if (type == "dmd") {
+      this.call("console_out", this.#convertImagePathToURL(table.DMDImagePath) )
+      return this.#convertImagePathToURL(table.DMDImagePath);
+
+    }
+    else if (type == "wheel")
+    {
+      this.call("console_out", this.#convertImagePathToURL(table.WheelImagePath) )
+      return this.#convertImagePathToURL(table.WheelImagePath);
+    }
+  }
+
+  // send a message to all windows except "self"
+  sendMessageToAllWindows(message) {
+    this.call("send_event_all_windows", message);
+  }
+
+  // launch a table
+  async launchTable(index) {
+    this.gamepadEnabled = false;
+    await this.call("launch_table", index);
+    this.call("console_out", "vpinfe-core returning")
+    this.gamepadEnabled = true;
+  }
+
+// **********************************************
+// private functions
+// **********************************************
+
+  async #onPyWebviewReady() {
+    console.log("pywebview is ready!");
+    await this.#loadMonitors();
+    await this.#getTableData();
+    
+    // only run on the table window.. Its the master controller for all screens/windows
+    if(await vpin.call("get_my_window_name") == "table") {
+      await this.#initGamepadMapping();
+      this.#updateGamepads();           // No await needed here — runs loop
+    }
+  }
+
+  async #getTableData() {
+    this.tableData = JSON.parse(await window.pywebview.api.get_tables());
+  }
+
+  async #loadMonitors() {
+    this.monitors = await window.pywebview.api.get_monitors();
+  }
+
+  // Gamepad handling
+  async #initGamepadMapping () {
+    const joymap = await this.call("get_joymaping");
+    this.joyButtonMap = Object.fromEntries(
+      Object.entries(joymap).map(([key, val]) => [val, key])
+    );
+  }
+
+  async #onButtonPressed(buttonIndex, gamepadIndex) {
+    const action = this.joyButtonMap[buttonIndex.toString()];
+    if (action) {
+      if (action === "joyexit" && windowName == "table") {
+        window.pywebview.api.close_app();
+      }
+      else {
+        this.inputHandlers.forEach(handler => handler(action));
+      }
+    }
+  }
+
+   #updateGamepads() {
     if (this.gamepadEnabled){ 
       const gamepads = navigator.getGamepads();
       for (let i = 0; i < gamepads.length; i++) {
@@ -89,94 +151,24 @@ class VPinFECore {
 
           if (isPressed && !wasPressed) {
             this.call("console_out", "Button: "+index);
-            this.onButtonPressed(index, i); // new press
+            this.#onButtonPressed(index, i); // new press
           }
-
           this.previousButtonStates[i][index] = isPressed;
         });
       }
     }
-  requestAnimationFrame(() => this.updateGamepads());
-}
-
-  async onButtonPressed(buttonIndex, gamepadIndex) {
-    const action = this.joyButtonMap[buttonIndex.toString()];
-    if (action) {
-      if (action === "joyexit" && windowName == "table") {
-        window.pywebview.api.close_app();
-      }
-      else {
-        this.inputHandlers.forEach(handler => handler(action));
-      }
-    }
+  requestAnimationFrame(() => this.#updateGamepads());
   }
 
-  // ************************************
-  // Keybaord input processing to handlers
-
-  onKeyDown(e) {
-    if (e.key === 'Escape' && window.pywebview) {
-      window.pywebview.api.close_app();
-    }
-  }
-
-  // **********************************************
-  // API Calls
-
-  async call(method, ...args) {
-  if (window.pywebview && window.pywebview.api && typeof window.pywebview.api[method] === 'function') {
-    return await window.pywebview.api[method](...args);
-  } else {
-    const errMsg = `Method ${method} does not exist on window.pywebview.api`;
-    console.error(errMsg);
-    throw new Error(errMsg);
-  }
-}
-
-  convertImagePathToURL(localPath) {
-     if (!localPath || typeof localPath !== 'string') {
-    return "../../images/file_missing.png";  // fallback default
+  // convert the hard full local path to the web servers url map
+  #convertImagePathToURL(localPath) {
+    if (!localPath || typeof localPath !== 'string') {
+      return "../../images/file_missing.png";  // fallback default
     }
     const parts = localPath.split('/');
     const dir = parts[parts.length - 2];     // second-to-last part = folder
     const file = parts[parts.length - 1];    // last part = filename
     return `http://127.0.0.1:8000/tables/${encodeURIComponent(dir)}/${encodeURIComponent(file)}`;
-}
-
-  getImageURL(index, type) {
-    const table = this.tableData[index];
-    if (type == "table") {
-      this.call("console_out", this.convertImagePathToURL(table.TableImagePath) )
-      return this.convertImagePathToURL(table.TableImagePath);
-    }
-    else if (type == "bg") {
-      this.call("console_out", this.convertImagePathToURL(table.BGImagePath) )
-      return this.convertImagePathToURL(table.BGImagePath);
-
-    } 
-    else if (type == "dmd") {
-      this.call("console_out", this.convertImagePathToURL(table.DMDImagePath) )
-      return this.convertImagePathToURL(table.DMDImagePath);
-
-    }
-    else if (type == "wheel")
-    {
-      this.call("console_out", this.convertImagePathToURL(table.WheelImagePath) )
-      return this.convertImagePathToURL(table.WheelImagePath);
-
-    }
-    
-  }
-
-  sendMessageToAllWindows(message) {
-    this.call("send_event_all_windows", message);
-  }
-
-  async launchTable(index) {
-    this.gamepadEnabled = false;
-    await this.call("launch_table", index);
-    this.call("console_out", "vpinfe-core returning")
-    this.gamepadEnabled = true;
   }
 
 }
