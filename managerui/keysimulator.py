24e5eaf453e752b3e5783e45409387dae488ac26
superhac <superhac007@gmail.com>
2026-02-02 07:44:23 -0600

    Fixed remote control issues on MAC..  Thanks to @jsm174

--- managerui/keysimulator.py
+++ managerui/keysimulator.py
@@ -1,10 +1,50 @@
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key
 import time
 import re
 from platformdirs import user_config_dir
 from pathlib import Path
 from common.iniconfig import IniConfig
 import sys
+
+if sys.platform == "darwin":
+    from Quartz import (
+        CGEventCreateKeyboardEvent,
+        CGEventPost,
+        kCGHIDEventTap,
+    )
+
+    PYNPUT_TO_MACOS_KEYCODE = {
+        Key.enter: 36, Key.esc: 53, Key.backspace: 51, Key.tab: 48, Key.space: 49,
+        Key.f1: 122, Key.f2: 120, Key.f3: 99, Key.f4: 118, Key.f5: 96, Key.f6: 97,
+        Key.f7: 98, Key.f8: 100, Key.f9: 101, Key.f10: 109, Key.f11: 103, Key.f12: 111,
+        Key.home: 115, Key.page_up: 116, Key.delete: 117, Key.end: 119, Key.page_down: 121,
+        Key.right: 124, Key.left: 123, Key.down: 125, Key.up: 126,
+        Key.ctrl_l: 59, Key.shift_l: 56, Key.alt_l: 58, Key.cmd: 55,
+        Key.ctrl_r: 59, Key.shift_r: 56, Key.alt_r: 58,
+        'a': 0, 'b': 11, 'c': 8, 'd': 2, 'e': 14, 'f': 3, 'g': 5, 'h': 4,
+        'i': 34, 'j': 38, 'k': 40, 'l': 37, 'm': 46, 'n': 45, 'o': 31, 'p': 35,
+        'q': 12, 'r': 15, 's': 1, 't': 17, 'u': 32, 'v': 9, 'w': 13, 'x': 7,
+        'y': 16, 'z': 6,
+        '1': 18, '2': 19, '3': 20, '4': 21, '5': 23, '6': 22, '7': 26, '8': 28,
+        '9': 25, '0': 29,
+        '-': 27, '=': 24, '[': 33, ']': 30, '\\': 42, ';': 41, "'": 39,
+        '`': 50, ',': 43, '.': 47, '/': 44,
+    }
+
+    class QuartzKeyboardController:
+        def press(self, key):
+            keycode = PYNPUT_TO_MACOS_KEYCODE.get(key)
+            if keycode is not None:
+                event = CGEventCreateKeyboardEvent(None, keycode, True)
+                CGEventPost(kCGHIDEventTap, event)
+
+        def release(self, key):
+            keycode = PYNPUT_TO_MACOS_KEYCODE.get(key)
+            if keycode is not None:
+                event = CGEventCreateKeyboardEvent(None, keycode, False)
+                CGEventPost(kCGHIDEventTap, event)
+else:
+    from pynput.keyboard import Controller as QuartzKeyboardController
 
 
 class KeySimulator:
@@ -56,9 +96,11 @@ class KeySimulator:
         69: Key.f12,
 
         # Navigation        
-       
+        74: Key.home,
+        75: Key.page_up,
         76: Key.delete,
         77: Key.end,
+        78: Key.page_down,
         79: Key.right,
         80: Key.left,
         81: Key.down,
@@ -81,9 +123,6 @@ class KeySimulator:
                 70: Key.print_screen,
                 72: Key.pause,
                 73: Key.insert,
-                74: Key.home,
-                75: Key.page_up,
-                78: Key.page_down,
         })
     
     # Pinmame 
@@ -99,6 +138,7 @@ class KeySimulator:
 
     def __init__(self, debug=False):
         self.debug = debug
+        self._keyboard = None
 
         config_dir = Path(user_config_dir("vpinfe", "vpinfe"))
         config_path = config_dir / "vpinfe.ini"
@@ -125,7 +165,11 @@ class KeySimulator:
             for name, key in self.pynput_mappings.items():
                 print(f"  {name}: {key}")
 
-        self.keyboard = Controller()
+    @property
+    def keyboard(self):
+        if self._keyboard is None:
+            self._keyboard = QuartzKeyboardController()
+        return self._keyboard
 
     # ------------------------------------------------------------------
     # Public helpers
