925fa07ec2e6892907b0d960d2e3af42c2cd2e2d
superhac <superhac007@gmail.com>
2026-02-06 22:09:36 -0600

    remote:::vpinfe: fix linux shutdown command. #3

--- managerui/pages/remote.py
+++ managerui/pages/remote.py
@@ -274,7 +274,7 @@ def _shutdown_system():
         subprocess.Popen(['osascript', '-e', 'tell app "System Events" to shut down'])
     else:
         # Linux: use systemctl with -i flag to ignore inhibitors (like GNOME session)
-        subprocess.Popen(['systemctl', 'poweroff', '-i'])
+        subprocess.Popen(['sudo', 'shutdown', '-h', 'now'])
 
     # Close VPinFE windows after issuing shutdown
     for window in webview.windows:
