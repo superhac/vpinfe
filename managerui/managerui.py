23ad68f2181700419b4204f6b87a334e93770e7e
Eric Brousseau <emb417@gmail.com>
2026-04-12 16:28:36 -0700

    fix to show remote with collapsed menu

--- managerui/managerui.py
+++ managerui/managerui.py
@@ -376,13 +376,11 @@ def build_app():
             nav_panel.style(add='width: 220px;', remove='width: 56px;')
             nav_state['nav_content'].classes(remove='nav-collapsed')
             nav_state['nav_label'].set_visibility(True)
-            nav_state['remote_container'].set_visibility(True)
             content_container.style(add='margin-left: 206px; width: calc(100vw - 220px); max-width: calc(100vw - 220px);', remove='margin-left: 56px; width: calc(100vw - 56px); max-width: calc(100vw - 56px);')
         else:
             nav_panel.style(add='width: 56px;', remove='width: 220px;')
             nav_state['nav_content'].classes(add='nav-collapsed')
             nav_state['nav_label'].set_visibility(False)
-            nav_state['remote_container'].set_visibility(False)
             content_container.style(add='margin-left: 40px; width: calc(100vw - 56px); max-width: calc(100vw - 56px);', remove='margin-left: 220px; width: calc(100vw - 220px); max-width: calc(100vw - 220px);')
 
     with nav_panel:
